4fec9a8efbb5c3c27311d6a53e88920b8fd849f9
Sourav <souravbhardwaj997@gmail.com>
2025-11-06 15:39:59 +0530

    added tabs, label, input, dialog, and created not found file and created profile page

--- src/components/Navbar/DesktopNavbar.tsx
+++ src/components/Navbar/DesktopNavbar.tsx
@@ -29,7 +29,12 @@ export async function DesktopNavbar() {
             </Link>
           </Button>
           <Button variant="ghost" className="flex items-center gap-2" asChild>
-            <Link href={`/profile/`}>
+            <Link
+              href={`/profile/${
+                user.username ??
+                user.emailAddresses[0].emailAddress.split("@")[0]
+              }`}
+            >
               <UserIcon className="w-4 h-4" />
               <span className="hidden lg:inline">Profile</span>
             </Link>
